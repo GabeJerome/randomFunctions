5bb6f3bf328b7a5a585a50575e0cefb1e58ebcf6
Gabriel Jerome <Gabriel.Jerome@mines.sdsmt.edu>
2022-08-13 11:26:40 -0500

    almost complete

--- toBinary/toBinary.cpp
+++ toBinary/toBinary.cpp
@@ -6,6 +6,8 @@ void txtToBinary( string fileName )
 {
     ifstream fin;
     ofstream fout;
+    string binFileName;
+    char name[20];
 
     if ( fileName.substr( fileName.size( ) - 4, 4 ) != ".txt" )
     {
@@ -20,7 +22,23 @@ void txtToBinary( string fileName )
         return;
     }
 
+    binFileName = fileName.substr( 0, fileName.find( '.' ) ) + ".bin";
 
+    fout.open( binFileName, ios::binary | ios::trunc | ios::out );
+    if ( !fout.is_open( ) )
+    {
+        cout << "Could not open file: " << binFileName << "." << endl;
+        return;
+    }
+
+    //not reading here
+    while ( fin >> name )
+    {
+        fout.write( (char *)&name, sizeof( name ) );
+    }
+
+    fin.close( );
+    fout.close( );
 }
 
 
